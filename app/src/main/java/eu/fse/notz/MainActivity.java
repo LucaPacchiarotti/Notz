e825aa65592a5f325a0bba1c8f57d7b525c7a860
LucaPacchiarotti <pacchiarotti.luca@cfp-pio.com>
2018-04-12 18:57:05 +0200

    project stracter

---
+++ app/src/main/java/eu/fse/notz/MainActivity.java
@@ -0,0 +1,36 @@
+package eu.fse.notz;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+/**
+ * Created by Amministratore on 12/04/2018.
+ */
+
+public class MainActivity extends AppCompatActivity {
+    String [] myDataset = {"nota 1", "nota 2", "nota 3"};
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mRecyclerView = (RecyclerView) findViewById(R.id.notes_rv);
+
+        mRecyclerView.setHasFixedSize(true);
+
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        mAdapter = new NotesAdapter (myDataset);
+        mRecyclerView.setAdapter(mAdapter);
+    }
+}
---
+++ app/src/main/java/eu/fse/notz/Note.java
@@ -0,0 +1,10 @@
+package eu.fse.notz;
+
+import android.app.Activity;
+
+/**
+ * Created by Amministratore on 12/04/2018.
+ */
+
+public class Note {
+}
